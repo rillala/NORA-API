e0b5ee2bbca320ce1c0bbaa8c35f10b5a4bd78d3
Orilla_Lin <142280255+rillala@users.noreply.github.com>
2024-02-05 14:54:15 +0800

    add connect DB file

---
+++ connect_chd104g1.php
@@ -0,0 +1,11 @@
+<?php
+	$dbname = "tibamefe_chd104g1";
+	$user = "tibamefe_since2021";
+	$password = "vwRBSb.j&K#E";
+	$port = 3306;
+
+	$dsn = "mysql:host=localhost;port={$port};dbname=$dbname;charset=utf8";
+	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
+	//建立pdo物件
+	$pdo = new PDO($dsn, $user, $password, $options);
+?>
