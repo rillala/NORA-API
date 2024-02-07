6adaa76e75b0d341eabc15c35c7942b0528e1d7f
ortis88 <caesar0718@gmail.com>
2024-02-07 17:04:06 +0800

    後台管理員維修

--- phps/getadmin.php
+++ phps/getadmin.php
@@ -4,17 +4,16 @@
 // header('Content-Type: application/json;charset=UTF-8');
 
 try{
-
     // $dbname = "nora";
     // $user = "root";
     // $password = "";
     // $port = 3306;
-
+    
     // $dsn = "mysql:host=localhost;port={$port};dbname=$dbname;charset=utf8";
     // $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
     // //建立pdo物件
     
-    $pdo = new PDO($dsn, $user, $password, $options);
+    // $pdo = new PDO($dsn, $user, $password, $options);
 
     require_once("./connect_chd104g1.php");
 
