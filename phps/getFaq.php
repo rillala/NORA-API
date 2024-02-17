d2670ec1d95ffb8a8ef8f071960d941fbae26450
nan-sansan <a85124236@gmail.com>
2024-02-17 14:23:05 +0800

    更新FaqPHP

--- phps/getFaq.php
+++ phps/getFaq.php
@@ -1,24 +1,12 @@
 <?php
     header("Access-Control-Allow-Origin: *"); // 允許所有來源
     header('Content-Type: application/json;charset=UTF-8');
-    
-    $dbname = "nora";
-    $user = "root";
-    $password = "";
-    $port = 3306;
 
-    $dsn = "mysql:host=localhost;port={$port};dbname=$dbname;charset=utf8";
-    $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
-    //建立pdo物件
+    require_once("./connect_chd104g1.php");
     
-    $pdo = new PDO($dsn, $user, $password, $options);
-    //要用require_once連結到另一個php檔案的connect內容
-
     try{
-
-    // require_once("./connect_chd104g1.php");    
-
     $sql = "select * from `faq_management`";
+
     $faq=$pdo->prepare($sql);
     $faq->execute();
     
@@ -32,4 +20,6 @@
     }catch (PDOException $e) {
 		echo "錯誤 : ", $e->getMessage(), "<br>";
 	}
+
+    ini_set("display_errors","On")
 ?>
