f8920c3cec249fe90cd4286b80291c2d6765c9d5
tiffany1029tw <tiffany1029tw@gmail.com>
2024-02-19 13:21:31 +0800

    更改php檔名

--- phps/getNews.php
+++ phps/getNews.php
@@ -1,22 +1,26 @@
 <?php
     header("Access-Control-Allow-Origin: *"); // 允許所有來源
     header('Content-Type: application/json;charset=UTF-8');
-    
-    $dbname = "nora";
-    $user = "root";
-    $password = "";
-    $port = 3306;
 
-    $dsn = "mysql:host=localhost;port={$port};dbname=$dbname;charset=utf8";
-    $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
-    //建立pdo物件
+    //連線到資料庫
+    require_once("./connect_chd104g1.php");
     
-    $pdo = new PDO($dsn, $user, $password, $options);
+    // $dbname = "nora";
+    // $user = "root";
+    // $password = "";
+    // $port = 3306;
+
+    // $dsn = "mysql:host=localhost;port={$port};dbname=$dbname;charset=utf8";
+    // $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
+    // //建立pdo物件
+    
+    // $pdo = new PDO($dsn, $user, $password, $options);
     //要用require_once連結到另一個php檔案的connect內容
 
     try{
 
-    $sql = "select article_id , title , create_date , status from news";
+    // $sql = "SELECT article_id , title , create_date , status from news";
+    $sql = "SELECT * from news";
     $news=$pdo->prepare($sql);
     $news->execute();
     
