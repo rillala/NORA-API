75e4da28f4708cef8c1f6bd820fabd6c877d94f1
tiffany1029tw <tiffany1029tw@gmail.com>
2024-02-23 14:33:09 +0800

    更改editArticle 為 editData

--- phps/editNews.php
+++ phps/editNews.php
@@ -7,7 +7,7 @@ header('Content-Type: application/json;charset=UTF-8');
 //連線到demo資料庫
 require_once("./connect_chd104g1.php");
 
-$editArticle = json_decode(file_get_contents("php://input"), true);
+$editData = json_decode(file_get_contents("php://input"), true);
 
 try {
 
@@ -15,14 +15,14 @@ try {
     $sql = "UPDATE news SET title = :title, content = :content, img1 = :img1, img2 = :img2, img3 = :img3, status = :status";
 
     // 判斷是否需要更新圖片
-    if ($editArticle["img1"] !== "") {
+    if ($editData["img1"] !== "") {
         // 根據前端發送的數據中的img1、img2、img3的值來判斷是否更新圖片
         $sql .= ", img1 = :img1";
     }
-    if ($editArticle["img2"] !== "") {
+    if ($editData["img2"] !== "") {
         $sql .= ", img2 = :img2";
     }
-    if ($editArticle["img3"] !== "") {
+    if ($editData["img3"] !== "") {
         $sql .= ", img3 = :img3";
     }
 
@@ -33,22 +33,22 @@ try {
     $stmt = $pdo->prepare($sql);
     
     // 綁定參數到每一個項目
-    $stmt->bindValue(":title", $editArticle["title"]);
-    $stmt->bindValue(":content", $editArticle["content"]);
-    $stmt->bindValue(":img1", $editArticle["img1"]);
-    $stmt->bindValue(":img2", $editArticle["img2"]);
-    $stmt->bindValue(":img3", $editArticle["img3"]);
-    $stmt->bindValue(":status", $editArticle["status"]);
+    $stmt->bindValue(":title", $editData["title"]);
+    $stmt->bindValue(":content", $editData["content"]);
+    $stmt->bindValue(":img1", $editData["img1"]);
+    $stmt->bindValue(":img2", $editData["img2"]);
+    $stmt->bindValue(":img3", $editData["img3"]);
+    $stmt->bindValue(":status", $editData["status"]);
 
     // 只有當對應的欄位值不為空時才綁定參數
-    if ($editArticle["img1"] !== "") {
-        $stmt->bindValue(":img1", $editArticle["img1"]);
+    if ($editData["img1"] !== "") {
+        $stmt->bindValue(":img1", $editData["img1"]);
     }
-    if ($editArticle["img2"] !== "") {
-        $stmt->bindValue(":img2", $editArticle["img2"]);
+    if ($editData["img2"] !== "") {
+        $stmt->bindValue(":img2", $editData["img2"]);
     }
-    if ($editArticle["img3"] !== "") {
-        $stmt->bindValue(":img3", $editArticle["img3"]);
+    if ($editData["img3"] !== "") {
+        $stmt->bindValue(":img3", $editData["img3"]);
     }
     
     // 執行sql指令
