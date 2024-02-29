f087afc2e623aea9f9134cfc1259e1382af5722e
Orilla_Lin <142280255+rillala@users.noreply.github.com>
2024-02-29 16:33:01 +0800

    會員修正 營位訂單取得

--- phps/logout.php
+++ phps/logout.php
@@ -10,62 +10,25 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header('Content-Type: application/json;charset=UTF-8');
 
-// 如果是 OPTIONS 請求，直接返回成功
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    exit(0);
-}
-
 require_once("./connect_chd104g1.php");
+$Data = json_decode(file_get_contents("php://input"), true);
 
-// 引入 JWT 類
-require_once '../vendor/autoload.php';
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-
-try {
-    // 從 HTTP Authorization Header 獲取 JWT token
-    $token = null;
-    $authHeader = $_SERVER['HTTP_AUTHORIZATION'] ?? '';
-
-    // echo $authHeader;
-    
-    if (preg_match('/Bearer\s(\S+)/', $authHeader, $matches)) {
-        $token = $matches[1];
-    }
-    
-    if (!$token) {
-        throw new Exception('Token not provided.');
-    }
-    
-    // 使用實際的密鑰解碼 JWT token 以獲取用戶ID
-    $decoded = JWT::decode($token, new Key("hi_this_is_nora_camping_project_for_CHD104g1", 'HS256'));
-    
-    $member_id = $decoded->sub;
-
+try {   
     // 將資料庫中的 token 欄位清空
-    $updateTokenSql = "UPDATE members SET token = NULL WHERE member_id = ?";
+    $updateTokenSql = "UPDATE members SET token = NULL WHERE member_id = :member";
     $updateStmt = $pdo->prepare($updateTokenSql);
-    $updateStmt->execute([$member_id]);
+    $updateStmt->bindValue(":member", $Data["data"]);   
+    $updateStmt->execute();
 
-    if ($updateStmt->rowCount() > 0) {
-        // Token 清除成功
-        http_response_code(200);
-        echo json_encode(['error' => false, 'message' => '登出成功'],JSON_UNESCAPED_UNICODE);
-    } else {
-        // Token 清除失敗
-        http_response_code(400); 
-        echo json_encode(['error' => true, 'message' => '登出失敗：未找到用戶或 token 已經清空'],JSON_UNESCAPED_UNICODE);
-    }
+    //準備要回傳給前端的資料
+    $result = ["error" => false, "message"=>"成功清空token"];
+
+
+    //準備要回傳給前端的資料
 } catch (PDOException $e) {
-    // 資料庫連接或查詢錯誤
-    http_response_code(500);
-    echo json_encode(['error' => true, 'message' => '資料庫錯誤：' . $e->getMessage()]);
-} catch (Exception $e) {
-    // 其他錯誤，比如 JWT 處理時出錯
-    http_response_code(401);
-    echo json_encode(['error' => true, 'message' => '服務器錯誤：' . $e->getMessage()]);
+    $result = ["error" => true, "message"=>$e->getMessage()];
 }
 
-// 結束輸出緩衝
-// ob_end_flush();
+//回傳資料給前端
+echo json_encode($result);
 ?>
